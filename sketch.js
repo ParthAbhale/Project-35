81cd633829cfff4d3ae0c5e8c40a0577761dbf31
ParthAbhale <72497177+ParthAbhale@users.noreply.github.com>
2020-12-05 13:59:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var dog,dogImg,dog2Img,database,foodStock;
+var Foods = 20;
+function preload()
+{
+  dogImg = loadImage("dogImg.png")
+  dog2Img = loadImage("dogImg1.png")
+}
+
+function setup() {
+  createCanvas(800, 800);
+  database = firebase.database()
+  foodStock = database.ref("Food")
+  foodStock.on("value",redStock)
+  foodStock.set(20)
+  dog = createSprite(width/2,height/2+50,10,10)
+  dog.addImage(dogImg)
+  dog.scale=0.4
+}
+
+
+function draw() {  
+ background("green")
+
+ 
+
+ if (keyWentDown(UP_ARROW)){
+   writeStocks(Foods)
+  dog.addImage(dog2Img)
+ } if (keyWentUp(UP_ARROW)){
+  //  writeStocks(Foods)
+  dog.addImage(dogImg)
+ }
+
+ if (Foods === 0){
+   Foods = 20
+ }
+
+ drawSprites();
+ textSize(30)
+ fill("yellow")
+ text("Food Remeaning: "+Foods,width/2-100,height/2-100)
+ text("Note: Press UP_ARROW Key To Feed Drogo Milk",width/2-350,30)
+}
+
+function redStock(data){
+  Foods=data.val();
+
+}
+
+function writeStocks(x){
+    if (x<=0){
+        x=0
+    }else{
+      x=x-1
+    }
+    database.ref('/').update({
+      Food:x
+    })
+  
+}
+
+
